e0e15e719d364e2ee5c5bdb238c99c7fd46f9d5c
=Seunghwa Cho <chobocho99@gmail.com>
2018-05-13 18:11:39 +0900

    Update code

--- src/ChoboFileManagerPanel.py
+++ src/ChoboFileManagerPanel.py
@@ -4,6 +4,7 @@ import FileListCtrl
 import UrlListCtrl
 import FileManager
 import UrlManager
+import os
 
 class ChoboFileManagerPanel(wx.Panel):
     def __init__(self, *args, **kw):
@@ -35,8 +36,60 @@ class ChoboFileManagerPanel(wx.Panel):
         elif self.fileManager.updateCurrentFolder(strUrl) == True:
             self.fileList.update(self.fileManager.getFileList())
 
+        self.urlText.SetValue(self.fileManager.getCurrentDir())
+
+    def onRunCmd(self, evt):
+        tmpCmd = self.cmdText.GetValue().strip()
+        self.cmdText.SetValue("")
+
+        print ("run cmd " + tmpCmd)
+        if (tmpCmd.lower() == "update"):
+            self.fileList.update(self.fileManager.getFileList())
         else:
-            self.urlText.SetValue(self.fileManager.getCurrentDir())
+            os.system("start " + tmpCmd)
+
+    def on_runexe(self, exefile):
+        print ("run " + exefile)
+        os.system("start " + exefile)
+
+    def on_runPython(sefl, pythonFile):
+        print ("run python " + pythonFile)
+        os.system("start python " + pythonFile)
+
+    def on_runtxt(self, txtfile):
+        print ("run " + txtfile)
+        os.system("start notepad " + txtfile)
+
+    def handleFileListEvnet(self, url):
+        print ("Event")
+
+        currdir = self.fileManager.getCurrentDir()
+        filename = url[1:-1]
+        fullfilename = os.path.join(currdir, filename)
+        isFolder = os.path.isdir(fullfilename)
+
+        if (url == ".."):
+            if self.fileManager.updateCurrentFolder(url) == True:
+                self.fileList.update(self.fileManager.getFileList())
+
+        elif (url[0] == '[' and isFolder == True):
+            if self.fileManager.updateCurrentFolder(fullfilename) == True:
+                self.fileList.update(self.fileManager.getFileList())
+ 
+        elif "exe." == url[:-5:-1] or \
+             "tab." == url[:-5:-1]:
+            self.on_runexe(url)
+     
+        elif ("yp." == url[:-4:-1].lower() or \
+              "wyp." == url[:-5:-1].lower()):
+            self.on_runPython(url)
+
+        elif ("txt." == url[:-5:-1].lower() or \
+              "pac." == url[:-5:-1].lower() or \
+              "ppc." == url[:-5:-1].lower() or \
+              "lmx." == url[:-5:-1].lower() or \
+              "gol." == url[:-5:-1].lower()):
+            self.on_runtxt(url)
 
     def drawUI(self):
         print ("drawUI")
@@ -76,21 +129,27 @@ class ChoboFileManagerPanel(wx.Panel):
                                  #| wx.LC_SINGLE_SEL
                                  )
         self.fileList.update(self.fileManager.getFileList())
+        self.fileList.setPanel(self)
         sizer.Add(self.fileList, 1, wx.EXPAND)
 
         ##
         fileMngBtnBox = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.cmdText = wx.TextCtrl(self, -1, " ",size=(400,-1))
+        
+        self.cmdLbl = wx.StaticText(self, -1, "Cmd")
+        fileMngBtnBox.Add(self.cmdLbl, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+        self.cmdText = wx.TextCtrl(self, style = wx.TE_PROCESS_ENTER|wx.TE_MULTILINE,size=(500,25))
+        self.cmdText.Bind(wx.EVT_TEXT_ENTER, self.onRunCmd)
+        #self.cmdText = wx.TextCtrl(self, -1, " ",size=(400,-1))
         fileMngBtnBox.Add(self.cmdText, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
-
-        self.cmdBtn = wx.Button(self, 10, "Run", size=(30,30))
-        self.cmdBtn.Bind(wx.EVT_BUTTON, self.OnClickGo)
-        fileMngBtnBox.Add(self.cmdBtn, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
-
-        self.cmdClearBtn = wx.Button(self, 10, "Clear", size=(30,30))
-        self.cmdClearBtn.Bind(wx.EVT_BUTTON, self.OnClickGo)
-        fileMngBtnBox.Add(self.cmdClearBtn, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
+        #
+        #self.cmdBtn = wx.Button(self, 10, "Run", size=(30,30))
+        #self.cmdBtn.Bind(wx.EVT_BUTTON, self.OnClickGo)
+        #fileMngBtnBox.Add(self.cmdBtn, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
+        #
+        #self.cmdClearBtn = wx.Button(self, 10, "Clear", size=(30,30))
+        #self.cmdClearBtn.Bind(wx.EVT_BUTTON, self.OnClickGo)
+        #fileMngBtnBox.Add(self.cmdClearBtn, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
 
         sizer.Add(fileMngBtnBox, 0, wx.GROW|wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
 
@@ -117,9 +176,9 @@ class ChoboFileManagerPanel(wx.Panel):
         self.urlSaveBtn.Bind(wx.EVT_BUTTON, self.OnClickGo)
         urlMngBtnBox.Add(self.urlSaveBtn, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
 
-        self.urlExportBtn = wx.Button(self, 10, "Export", size=(30,30))
-        self.urlExportBtn.Bind(wx.EVT_BUTTON, self.OnClickGo)
-        urlMngBtnBox.Add(self.urlExportBtn, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
+        #self.urlExportBtn = wx.Button(self, 10, "Export", size=(30,30))
+        #self.urlExportBtn.Bind(wx.EVT_BUTTON, self.OnClickGo)
+        #urlMngBtnBox.Add(self.urlExportBtn, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
 
         self.urlDeleteBtn = wx.Button(self, 10, "Delete", size=(30,30))
         self.urlDeleteBtn.Bind(wx.EVT_BUTTON, self.OnClickGo)
--- src/FileListCtrl.py
+++ src/FileListCtrl.py
@@ -6,12 +6,32 @@ class FileListCtrl(wx.ListCtrl, listmix.ListCtrlAutoWidthMixin):
                  size=wx.DefaultSize, style=0):
         wx.ListCtrl.__init__(self, parent, ID, pos, size, style)
         listmix.ListCtrlAutoWidthMixin.__init__(self)
+        self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.onItemSelected)
+        self.Bind(wx.EVT_LEFT_DCLICK, self.onDoubleClick)
         self.addColumn()
+		
+    def setPanel(self, panel):
+        self.panel = panel
 
     def addColumn(self):
         self.InsertColumn(0, "FileName", width=400)
         self.InsertColumn(1, "Size", width=100)
+        self.currentItem = -1
 
+    def getFileInfo(self, idx):
+        file = []
+        file.append(self.GetItem(idx, 0).GetText())
+        file.append(self.GetItem(idx, 0).GetText())
+        return file
+
+    def onItemSelected(self, evt):
+        self.currentItem = evt.Index
+
+    def onDoubleClick(self, evt):
+        if (self.currentItem != -1):
+            print ("DK: " + self.getFileInfo(self.currentItem)[0])
+            self.panel.handleFileListEvnet(self.getFileInfo(self.currentItem)[0])
+            
     def update(self, fileList):
         self.DeleteAllItems()
         for file in fileList:
@@ -20,5 +40,8 @@ class FileListCtrl(wx.ListCtrl, listmix.ListCtrlAutoWidthMixin):
             self.SetItem(index, 1, file[1])
             if index % 2 == 1:
                 self.SetItemBackgroundColour(index, "Light gray")
+            if "exe." == file[0][:-5:-1] or \
+               "tab." == file[0][:-5:-1]:
+                self.SetItemBackgroundColour(index, "Light blue")
 
  
